a7c8d435cafdc02fe4d3313b789be5821168b8ce
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-01-26 13:17:05 +0100

    Init commit

---
+++ user.rb
@@ -0,0 +1,11 @@
+class User
+  include Validator
+
+  attr_accessor :name
+  # Ta metoda musi zostać zaimplementowana w module
+  validate :name, LengthValidation.new(between: [5,10])
+
+  def save!
+    validate! # Ta metoda musi zostać zaimplementowana w module
+  end
+end
